272cecbc45fc8290c85c7f7a7922b5faa9ec656d
Adrian Schlatter <schlatter@phys.ethz.ch>
2017-03-18 22:35:17 +0100

    --subst ++++++++
    Added command-line argument --subst taking a (json) dictionary defining
    variable substitutions.

--- traveldiary2html/traveldiary2html.py
+++ traveldiary2html/traveldiary2html.py
@@ -3,22 +3,60 @@
 """
 
 from docutils.readers.standalone import Reader as Standalone
+from docutils import frontend
 from .utils import export
+from json import loads, JSONDecodeError
 
 
 @export
 class Reader(Standalone):
     """Standalone reader adding support for variable substitution at runtime"""
 
-    def __init__(self, parser=None, parser_name=None):
-        super().__init__(parser=parser, parser_name=parser_name)
-        self.varsubst = {'imp': 'where/my/images/are'}
+    settings_spec = (
+        'Variable-substituting Reader',
+        None,
+        (('Disable the promotion of a lone top-level section title to '
+          'document title (and subsequent section title to document '
+          'subtitle promotion; enabled by default).',
+          ['--no-doc-title'],
+          {'dest': 'doctitle_xform', 'action': 'store_false', 'default': 1,
+           'validator': frontend.validate_boolean}),
+         ('Disable the bibliographic field list transform (enabled by '
+          'default).',
+          ['--no-doc-info'],
+          {'dest': 'docinfo_xform', 'action': 'store_false', 'default': 1,
+           'validator': frontend.validate_boolean}),
+         ('Activate the promotion of lone subsection titles to '
+          'section subtitles (disabled by default).',
+          ['--section-subtitles'],
+          {'dest': 'sectsubtitle_xform', 'action': 'store_true', 'default': 0,
+           'validator': frontend.validate_boolean}),
+         ('Deactivate the promotion of lone subsection titles.',
+          ['--no-section-subtitles'],
+          {'dest': 'sectsubtitle_xform', 'action': 'store_false'}),
+         ('Define variable substitutions as a JSON dictionary.',
+          ['--subst'],
+          {'type': str, 'default': "{}", 'dest': 'varsubst'}),
+         ))
+
+    config_section = 'variable-substituting reader'
+    config_section_dependencies = ('standalone reader', 'readers')
 
     def read(self, source, parser, settings):
         self.source = source
         if not self.parser:
             self.parser = parser
+        try:
+            settings.varsubst = loads(settings.varsubst)
+        except JSONDecodeError as e:
+            raise JSONDecodeError('Unable to parse variable substitutions',
+                                  e.doc, e.pos)
+
         self.settings = settings
-        self.input = self.source.read().format(**self.varsubst)
+        self.input = self.source.read()
+        try:
+            self.input = self.input.format(**settings.varsubst)
+        except KeyError:
+            raise KeyError('Error during substitution of variables')
         self.parse()
         return self.document
