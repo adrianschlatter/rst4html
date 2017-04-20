c78739a0d41773ae9fd54ee64bb98586fc87d66b
Adrian Schlatter <schlatter@phys.ethz.ch>
2017-04-20 19:44:28 +0200

    Image Transformation Demo +++++++++++++++++++++++++
    Rotates (according to EXIF) and scales (according to .rst) images.
    
    Pure demo, ugly code with hard-coded variables, but it demonstrates what
    I want.

--- traveldiary2html/traveldiary2html.py
+++ traveldiary2html/traveldiary2html.py
@@ -12,6 +12,23 @@ from os.path import join, basename, exists, isdir
 from os import mkdir, listdir
 from shutil import copyfile
 
+# check for the Python Imaging Library
+try:
+    import PIL.Image
+    import PIL.ExifTags
+except ImportError:
+    try:  # sometimes PIL modules are put in PYTHONPATH's root
+        import Image
+        import ExifTags
+
+        class PIL(object):
+            pass  # dummy wrapper
+
+        PIL.Image = Image
+        PIL.ExifTags = ExifTags
+    except ImportError:
+        PIL = None
+
 
 @export
 class Reader(Standalone):
@@ -61,6 +78,8 @@ class Writer(HtmlWriter):
                           {'type': str, 'default': 'img'}),
                          ),)
 
+    config_section = 'html4css1 with image-copies writer'
+
     def __init__(self):
         super().__init__()
         self.translator_class = ModifiedHTMLTranslator
@@ -85,10 +104,45 @@ class ModifiedHTMLTranslator(HTMLTranslator):
     location and places them in a separate image directory.
     """
 
+    exiftrans = {1: (),
+                 2: (PIL.Image.FLIP_LEFT_RIGHT,),
+                 3: (PIL.Image.ROTATE_180,),
+                 4: (PIL.Image.FLIP_TOP_BOTTOM,),
+                 5: (PIL.Image.TRANSPOSE, ),
+                 6: (PIL.Image.ROTATE_270,),
+                 7: (PIL.Image.FLIP_LEFT_RIGHT, PIL.Image.ROTATE_270),
+                 8: (PIL.Image.ROTATE_90,)}
+
     def visit_image(self, node):
         uri = node['uri']
         newuri = join(self.settings.imgtargetdir, basename(uri))
         node['uri'] = newuri
-        copyfile(uri, newuri)
+        tlw = 600
+        # open image
+        img = PIL.Image.open(uri)
+
+        # determine required autorotation
+        tags = dict([(PIL.ExifTags.TAGS.get(k, k), v)
+                     for k, v in img._getexif().items()])
+        orientation = tags.get('Orientation', 1)  # default means 'upright'
+
+        # rotate and scale image
+        for op in self.exiftrans[orientation]:
+            img = img.transpose(op)
+        # now, the image is upright
+        # determine scaling from rst and image
+        width = img.width
+        newwidth = img.width
+        if node.hasattr('width'):
+            newwidth = node['width']
+            if newwidth[-1] == '%':
+                newwidth = int(float(newwidth[:-1]) * 1e-2 * tlw)
+        height = img.height
+        newheight = int(newwidth / width * height)
+        # XXX is exif orientation tag also correct now?
+        img = img.resize((newwidth, newheight), resample=PIL.Image.LANCZOS)
+        # store image
+        img.save(newuri)
+#        copyfile(uri, newuri)
 
         super().visit_image(node)
